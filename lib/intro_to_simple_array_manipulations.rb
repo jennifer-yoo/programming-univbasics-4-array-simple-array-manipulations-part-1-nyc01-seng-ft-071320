f4d87511cad8266bc45faac6253a00a9f25eed61
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-06-24 18:57:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,5 +11,13 @@ def using_pop(array)
 end
 
 def pop_with_args(array, number)
-  array.pop(2)
+  array.pop(number)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array, number)
+  array.shift(number)
 end
